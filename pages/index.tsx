13c52c08e933ce9a00017bcedc649462d229b618
Daniel Krajnak <dan.krajnak@gmail.com>
2021-12-11 20:14:20 -0500

    Geomoetry manipulation approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
-import { OrbitControls, Stage, Stats } from "@react-three/drei";
+import React, { Suspense, useRef } from "react";
+import {
+  OrbitControls,
+  Stage,
+  Stars,
+  Stats,
+  useHelper,
+} from "@react-three/drei";
 import Cloth from "../src/Cloth";
+import * as THREE from "three";
+import { PointLightHelper } from "three";
+
+const origin = new THREE.Vector3(0, 0, 0);
 
 const Home: NextPage = () => {
   return (
@@ -10,20 +20,59 @@ const Home: NextPage = () => {
       <Canvas dpr={[1, 2]}>
         <Stats showPanel={0} />
         <Suspense fallback={null}>
-          <OrbitControls />
-          <Stage
-            shadows
-            adjustCamera
-            intensity={1}
-            environment="city"
-            preset="rembrandt"
-          >
-            <Cloth />
-          </Stage>
+          <Inner />
         </Suspense>
       </Canvas>
     </div>
   );
 };
 
+const Inner = () => {
+  const pointLightRef = useRef();
+  const bluePointLightRef = useRef();
+  const greenPointLightRef = useRef();
+  useHelper(pointLightRef, PointLightHelper);
+  useHelper(bluePointLightRef, PointLightHelper);
+  useHelper(greenPointLightRef, PointLightHelper);
+  return (
+    <>
+      <OrbitControls
+        minDistance={30}
+        enableZoom={false}
+        enablePan={false}
+        maxPolarAngle={Math.PI / 2.5}
+        minPolarAngle={Math.PI / 4}
+      />
+      <Suspense fallback={null}>
+        <Stars
+          radius={100} // Radius of the inner sphere (default=100)
+          depth={50} // Depth of area where stars should fit (default=50)
+          count={5000} // Amount of stars (default=5000)
+          factor={4} // Size factor (default=4)
+          saturation={0} // Saturation 0-1 (default=0)
+          fade // Faded dots (default=false)
+        />
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.1, 0]}>
+          <planeGeometry args={[2000, 2000, 4]} />
+          <meshBasicMaterial color="black" />
+        </mesh>
+
+        <pointLight
+          position={[0, 5, 0]}
+          color="red"
+          ref={pointLightRef}
+          intensity={5}
+        />
+        <pointLight
+          position={[0, -5, 0]}
+          color="blue"
+          ref={bluePointLightRef}
+          intensity={5}
+        />
+
+        <Cloth />
+      </Suspense>
+    </>
+  );
+};
 export default Home;
--- src/Cloth.tsx
+++ src/Cloth.tsx
@@ -1,17 +1,44 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
-import fragmentShader from "./shaders/cloth/fragment.glsl";
-import vertexShader from "./shaders/cloth/vertex.glsl";
+import { PlaneBufferGeometry } from "three";
 
 const Cloth = () => {
+  const timeRef = useRef({ value: 0 });
+  const geometryRef = useRef<PlaneBufferGeometry>();
+  useFrame((state) => {
+    const time = state.clock.elapsedTime / 3;
+    if (geometryRef.current) {
+      const positions = geometryRef.current.getAttribute("position");
+      for (let i = 0; i < positions.count; i++) {
+        const newZ =
+          2 +
+          Math.sin((positions.getX(i) + time) * 3) +
+          Math.sin((positions.getY(i) + time) * 2);
+        positions.setZ(i, newZ);
+      }
+      positions.needsUpdate = true;
+      // geometryRef.current.computeVertexNormals();
+    }
+  });
   return (
-    <mesh rotation={[Math.PI / 2, 0, 0]}>
-      <shaderMaterial
-        side={THREE.DoubleSide}
-        vertexShader={vertexShader}
-        fragmentShader={fragmentShader}
-      />
-      <planeGeometry />
-    </mesh>
+    <>
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <meshStandardMaterial
+          metalness={0.8}
+          transparent
+          opacity={0.8}
+          side={THREE.DoubleSide}
+        />
+        <planeBufferGeometry args={[30, 30, 100, 100]} ref={geometryRef} />
+      </mesh>
+      {/* <lineSegments position={[0, 6.5, 0]}>
+        <edgesGeometry
+          args={[new THREE.BoxGeometry(30, 15, 30)]}
+        ></edgesGeometry>
+        <lineBasicMaterial color="white" />
+      </lineSegments> */}
+    </>
   );
 };
 
